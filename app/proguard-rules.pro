ae41f73b4defaca280d6bf44076fd66c2af7e192
梦里花 <wj1164664118@163.com>
2017-11-02 10:17:10 +0800

    [commit] 第一个版本(即有bug的版本)

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -23,3 +23,32 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+-keepattributes *Annotation*
+-dontwarn com.tencent.tinker.anno.AnnotationProcessor
+-keep @com.tencent.tinker.anno.DefaultLifeCycle public class *
+-keep public class * extends android.app.Application {
+    *;
+}
+
+-keep public class com.tencent.tinker.loader.app.ApplicationLifeCycle {
+    *;
+}
+-keep public class * implements com.tencent.tinker.loader.app.ApplicationLifeCycle {
+    *;
+}
+
+-keep public class com.tencent.tinker.loader.TinkerLoader {
+    *;
+}
+-keep public class * extends com.tencent.tinker.loader.TinkerLoader {
+    *;
+}
+
+-keep public class com.tencent.tinker.loader.TinkerTestDexLoad {
+    *;
+}
+
+#your dex.loader pattern here
+-keep class com.tencent.tinker.loader.**
+-keep class com.wj.tinker.SimpleTinkerInApplication
--- app/src/main/java/com/wj/tinkerdemo/MainActivity.java
+++ app/src/main/java/com/wj/tinkerdemo/MainActivity.java
@@ -1,13 +1,42 @@
 package com.wj.tinkerdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.wj.tinker.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    /**
+     * 初始化界面
+     */
+    private void initView() {
+        context = this;
+        textView = (TextView) findViewById(R.id.main_tv);
+    }
+
+    /**
+     * 点击按钮 加载patch
+     *
+     * @param view
+     */
+    public void loadPatch(View view) {
+        TinkerInstaller.onReceiveUpgradePatch(context,
+                Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed.apk");
     }
 }
